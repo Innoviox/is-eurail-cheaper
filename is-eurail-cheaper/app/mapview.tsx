85f38a159fe00db156aac09ab557015f7fc5d6a5
Simon Chervenak <sarras305@gmail.com>
2023-08-30 23:29:51 -0400

    LEGGS FUCKING GO

--- is-eurail-cheaper/app/mapview.tsx
+++ is-eurail-cheaper/app/mapview.tsx
@@ -1,27 +1,20 @@
 'use client'
 import React from "react";
-
-
+import { useEffect, useRef, useMemo } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 export default function MapView({latitude, longitude}: {latitude: number; longitude: number}) {
-    const ref = React.useRef(null);
+    const mapRef = useRef(null);
     const [map, setMap] = React.useState<google.maps.Map | null>(null);
-
-    React.useEffect(() => {
-        if (ref.current && !map) {
-            setMap(
-                new window.google.maps.Map(ref.current, {
-                    zoomControl: true,
-                    mapTypeControl: false,
-                    streetViewControl: false,
-                    center: {
-                        lat: latitude ?? 0,
-                        lng: longitude ?? 0,
-                    },
-                    zoom: 11,
-                })
-            );
-        }
-    }, [ref, map, latitude, longitude]);
-
-    return <div ref={ref} style={{height: '100%', width: '100%'}} />;
-};
+    useEffect(() => {
+        setMap( new window.google.maps.Map(mapRef.current, {
+                        zoomControl: true,
+                        mapTypeControl: false,
+                        streetViewControl: false,
+                        center: {
+                            lat: latitude ?? 0,
+                            lng: longitude ?? 0,
+                        },
+                        zoom: 11,}));
+    }, [latitude, longitude]);
+    return <div style={{height: "400px"}} ref={mapRef}/>;
+}
