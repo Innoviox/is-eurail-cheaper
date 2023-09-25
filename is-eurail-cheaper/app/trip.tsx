4d4899496e55324cce9b9fad1a276f130a5c5216
Simon Chervenak <sarras305@gmail.com>
2023-09-25 17:05:41 -0400

    add a level

--- is-eurail-cheaper/app/trip.tsx
+++ is-eurail-cheaper/app/trip.tsx
@@ -1,11 +1,27 @@
 import React, {useState, ChangeEvent, FormEvent, MouseEvent } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 export default function Trip({trip, open, onclick}: {trip: any; open: boolean, onclick: any}) {
 
     function openTrip() {
         return (
             <div className="open-trip">
-                {trip.name}
+                <nav className="level">
+                    <div className="level-left">
+                        <div className="level-item">
+                            <div>
+                                <FontAwesomeIcon icon={faArrowLeft} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="level-item has-text-centered">
+                        <div>
+                            <span>{trip.name}</span>
+                        </div>
+                    </div>
+                </nav>
             </div>
         )
     }
