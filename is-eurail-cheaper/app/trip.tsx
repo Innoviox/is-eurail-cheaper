a0e540922909f6ee8ab91c6af840530e4507463d
Simon Chervenak <sarras305@gmail.com>
2023-09-25 23:11:45 -0400

    use breadcrumbs

--- is-eurail-cheaper/app/trip.tsx
+++ is-eurail-cheaper/app/trip.tsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faTrain } from '@fortawesome/free-solid-svg-icons'
 
 export default function Trip({trip, open, onopen, onclose}: {trip: any; open: boolean, onopen: any, onclose: any}) {
+    const city = (idx: number) => [...trip.cities][idx][0];
+
     function openTrip() {
         return (
             <div className="open-trip">
@@ -26,7 +28,6 @@ export default function Trip({trip, open, onopen, onclose}: {trip: any; open: bo
                  <div>
                      {Array.from(trip.cities.keys(), (city: string, idx: number) => (
                          <div key={city}>
-                             <span>{city}</span><br />
                              {renderPrices(idx)}
                          </div>
                      ))}
@@ -38,22 +39,32 @@ export default function Trip({trip, open, onopen, onclose}: {trip: any; open: bo
     function renderPrices(idx: number) {
         if (idx < trip.cities.size - 1) {
             return (
-                <table className="table">
-                    <tbody>
-                    <tr>
-                        <td>
-                            <FontAwesomeIcon icon={faTrain} />
-                        </td>
-                        <td>{trip.prices.db[idx] === '+' ?
-                            <button className="button is-loading" disabled>Loading</button> :
-                            trip.prices.db[idx]}</td>
-                        <td>{trip.prices.eurail[idx] === '+' ?
-                            <button className="button is-loading" disabled>Loading</button> :
-                            trip.prices.eurail[idx]}</td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div>
+                    <nav className="breadcrumb has-succeeds-separator trip-title-breadcrumb" aria-label="breadcrumbs">
+                        <ul>
+                            <li><a href="#">{city(idx)}</a></li>
+                            <li><a href="#">{city(idx + 1)}</a></li>
+                        </ul>
+                    </nav>
+                    <table className="table">
+                        <tbody>
+                        <tr>
+                            <td>
+                                <FontAwesomeIcon icon={faTrain} />
+                            </td>
+                            <td>{trip.prices.db[idx] === '+' ?
+                                <button className="button is-loading" disabled>Loading</button> :
+                                trip.prices.db[idx]}</td>
+                            <td>{trip.prices.eurail[idx] === '+' ?
+                                <button className="button is-loading" disabled>Loading</button> :
+                                trip.prices.eurail[idx]}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             )
+        } else if (idx === trip.cities.size - 1) {
+            return <span>{city(idx)}</span>
         }
     }
 
