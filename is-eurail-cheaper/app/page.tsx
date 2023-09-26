79841ce45bd0c69bcd8de9ecb93f7f6476ff0439
Simon Chervenak <sarras305@gmail.com>
2023-09-25 22:30:33 -0400

    navbar

--- is-eurail-cheaper/app/page.tsx
+++ is-eurail-cheaper/app/page.tsx
@@ -7,16 +7,33 @@ import TripView from './tripview'
 
 export default function Home() {
   return (
-      <div id="container">
-          <div id="map">
-              {/*<Wrapper apiKey="AIzaSyCfa86sCHMFXR6ZHO-GqpUvFpffWCUroOc">*/}
-              <Wrapper apiKey="dead">
-                <MapView latitude={50} longitude={10} />
-              </Wrapper>
+      <main>
+          <nav className="navbar" role="navigation" aria-label="main navigation">
+              <div className="navbar-menu">
+                  <div className="navbar-start">
+                      <a className="navbar-item">
+                          Home
+                      </a>
+                  </div>
+
+                  <div className="navbar-end">
+                      <a className="navbar-item">
+                          Home
+                      </a>
+                  </div>
+              </div>
+          </nav>
+          <div id="container">
+              <div id="map">
+                  {/*<Wrapper apiKey="AIzaSyCfa86sCHMFXR6ZHO-GqpUvFpffWCUroOc">*/}
+                  <Wrapper apiKey="dead">
+                      <MapView latitude={50} longitude={10}/>
+                  </Wrapper>
+              </div>
+              <div id="trip">
+                  <TripView/>
+              </div>
           </div>
-          <div id="trip">
-              <TripView />
-          </div>
-      </div>
+      </main>
   )
 }
