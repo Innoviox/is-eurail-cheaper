076602b731d1f2f2bda1e475deccddc6ecd9a51e
Simon Chervenak <sarras305@gmail.com>
2023-10-14 15:58:17 -0400

    add city dot

--- is-eurail-cheaper/app/city.tsx
+++ is-eurail-cheaper/app/city.tsx
@@ -4,6 +4,9 @@ import React from "react";
 export default function City({name}: {name: string}) {
     return (
         // <button className="city button is-outlined is-rounded">{name}</button>
-        <span>{name}</span>
+        <div className="city">
+            <span className="dot" />
+            <span>{name}</span>
+        </div>
     )
 }
