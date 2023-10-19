51a1e5895cf54dc478bbaef47fa4de7b00caf143
Simon Chervenak <sarras305@gmail.com>
2023-10-18 20:40:40 -0400

    more colors

--- is-eurail-cheaper/app/colors.tsx
+++ is-eurail-cheaper/app/colors.tsx
@@ -3,7 +3,10 @@ const colors: string[] = [];
 export function initialize() {
     for (let i = 0; i < 1000; i++) {
         // colors.push(`#${Math.floor(Math.random()*16777215).toString(16)}`);
-        colors.push("red");
+        for (let i of ["#0a5fd0", "#ed0dba", "#6f2fe3", "#EE6C4D", "#293241"]) {
+            colors.push(i);
+        }
+        // colors.push("red");
     }
 }
 
