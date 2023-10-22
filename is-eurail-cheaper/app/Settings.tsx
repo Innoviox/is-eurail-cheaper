5032ecfba440c7843fe3304f1ac3967fe2e59967
Simon Chervenak <sarras305@gmail.com>
2023-10-22 00:12:00 -0400

    fix it using skills andl ogic

--- is-eurail-cheaper/app/Settings.tsx
+++ is-eurail-cheaper/app/Settings.tsx
@@ -1,22 +1,16 @@
+import {ChangeEvent, useState} from "react";
+
 export default function Settings({ visible, setVisible }:
                                  { visible: boolean, setVisible: (visible: boolean) => void }) {
+    let [weeks, setWeeks] = useState(2);
+
     return (
         <div className={"modal " + (visible ? "is-active" : "")}>
             <div className="modal-background"></div>
             <div className="modal-content">
                 <div className="background-white">
-                    <div className="field is-horizontal">
-                        <div className="field-label is-normal">
-                            <label className="label">Weeks 1</label>
-                        </div>
-                        <div className="field-body">
-                            <div className="field">
-                                <p className="control">
-                                    <input type="range" step={1} min={0} max={12} value={2} />
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <input type="range" step={1} min={0} max={12} value={weeks}
+                           onChange={(e: ChangeEvent<HTMLInputElement>) => setWeeks(parseInt(e.target.value))} />
                 </div>
             </div>
             <button className="modal-close is-large" aria-label="close" onClick={() => setVisible(false)}></button>
