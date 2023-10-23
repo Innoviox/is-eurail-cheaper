e8f821a1dd77eefe4b2119d93ff2d089ef15801f
Simon Chervenak <sarras305@gmail.com>
2023-10-22 22:41:18 -0400

    working on settings page

--- is-eurail-cheaper/app/components/Settings.tsx
+++ is-eurail-cheaper/app/components/Settings.tsx
@@ -15,10 +15,18 @@ export default function Settings({ visible, setVisible, setWeeksGlobal }:
         <div className={"modal " + (visible ? "is-active" : "")}>
             <div className="modal-background settings-background" onClick={close}></div>
             <div className="modal-content" id="settings">
-                <div className="background-white">
-                    <label className="label">{weeks} week{weeks === 1 ? "" : "s"}</label>
-                    <input type="range" step={1} min={0} max={12} value={weeks}
-                           onChange={(e: ChangeEvent<HTMLInputElement>) => setWeeksInternal(parseInt(e.target.value))} />
+                <div className="background-white settings-content">
+                    <div className="field is-horizontal">
+                        <div className="field-label is-normal">
+                            <label className="label">{weeks} week{weeks === 1 ? "" : "s"}</label>
+                        </div>
+                        <div className="field-body">
+                            <div className="field">
+                                <input type="range" step={1} min={0} max={12} value={weeks} id="weeks"
+                                       onChange={(e: ChangeEvent<HTMLInputElement>) => setWeeksInternal(parseInt(e.target.value))} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <button className="modal-close is-large" aria-label="close" onClick={close}></button>
