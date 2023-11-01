b9ae879b2fee271f271bab5c430a940bea26e73b
Simon Chervenak <sarras305@gmail.com>
2023-11-01 18:03:18 -0400

    add spaces and formatting

--- is-eurail-cheaper/app/components/modal/About.tsx
+++ is-eurail-cheaper/app/components/modal/About.tsx
@@ -19,7 +19,21 @@ export default function About({ visible, setVisible }:
                         <p>Thanks to <a href="https://github.com/juliuste">Julius Tens</a> for the inspiration and the stations search code.</p>
                         <p>Mapping data is from the <a href="https://developers.google.com/maps">Google Maps API</a>, integrated into React with help from <a href="https://github.com/leighhalliday/google-maps-threejs">Leigh Halliday</a>.</p>
                         <p>Pricing data is from the <a href="https://transport.rest/">transport.rest</a> and <a href="https://www.eurail.com/en">eurail</a> APIs.</p>
-                        <p>Thanks to these posts (<a href="https://stackoverflow.com/questions/71193818/react-onclick-argument-of-type-eventtarget-is-not-assignable-to-parameter-of-t">1</a>, <a href="https://stackoverflow.com/a/54292872/6342812">2</a>, <a href="https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258">3</a>, <a href="https://stackoverflow.com/questions/3793397/html5-canvas-drawimage-with-at-an-angle">4</a>, <a href="https://stackoverflow.com/questions/68324216/next-js-image-is-not-defined">5</a>, <a href="https://stackoverflow.com/questions/1776915/how-can-i-center-an-absolutely-positioned-element-in-a-div">6</a>, <a href="https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary">7</a>, <a href="https://www.coding-dude.com/wp/css/css-star/#:~:text=To%20make%20this%204%20points,and%20the%20other%20rotates%20%2D45deg%20.">8</a>, <a href="https://stackoverflow.com/questions/33766350/how-to-perfectly-center-a-plus-sign-in-a-circle-using-css">9</a>, <a href="https://stackoverflow.com/questions/22252472/how-can-i-change-the-color-of-an-svg-element">10</a>, <a href="https://stackoverflow.com/questions/37949981/call-child-method-from-parent">11</a>, <a href="https://www.reddit.com/r/nextjs/comments/qxyf2u/nextjs_fouc/">12</a>, <a href="https://stackoverflow.com/questions/57609931/next-js-with-fortawesome-and-ssr">13</a>) for helping me during the development process, as well as probably others that I forgot to save.</p>
+                        <p>Thanks to these posts (
+                            <a href="https://stackoverflow.com/questions/71193818/react-onclick-argument-of-type-eventtarget-is-not-assignable-to-parameter-of-t">1</a>,&nbsp;
+                            <a href="https://stackoverflow.com/a/54292872/6342812">2</a>,&nbsp;
+                            <a href="https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258">3</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/3793397/html5-canvas-drawimage-with-at-an-angle">4</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/68324216/next-js-image-is-not-defined">5</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/1776915/how-can-i-center-an-absolutely-positioned-element-in-a-div">6</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary">7</a>,&nbsp;
+                            <a href="https://www.coding-dude.com/wp/css/css-star/#:~:text=To%20make%20this%204%20points,and%20the%20other%20rotates%20%2D45deg%20.">8</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/33766350/how-to-perfectly-center-a-plus-sign-in-a-circle-using-css">9</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/22252472/how-can-i-change-the-color-of-an-svg-element">10</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/37949981/call-child-method-from-parent">11</a>,&nbsp;
+                            <a href="https://www.reddit.com/r/nextjs/comments/qxyf2u/nextjs_fouc/">12</a>,&nbsp;
+                            <a href="https://stackoverflow.com/questions/57609931/next-js-with-fortawesome-and-ssr">13</a>
+                            ) for helping me during the development process, as well as probably others that I forgot to save.</p>
                         <p>Please let me know at sarras305-at-gmail or at the <a href="https://github.com/Innoviox/is-eurail-cheaper/issues">issues board</a> of any bugs or feature recommendations.</p>
                     </div>
                 </div>
