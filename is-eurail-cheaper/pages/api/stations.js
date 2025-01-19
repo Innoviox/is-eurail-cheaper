6cbd94fce3d8fdbe89866b75b55b79facafc40bd
Simon Chervenak <sarras305@gmail.com>
2025-01-19 00:04:00 -0500

    fix backend for now

--- is-eurail-cheaper/pages/api/stations.js
+++ is-eurail-cheaper/pages/api/stations.js
@@ -1,88 +1,114 @@
-import cleanStationName from 'db-clean-station-name'
-import isUicLocationCode from 'is-uic-location-code'
-import countries from 'i18n-iso-countries'
-import fetch from 'node-fetch'
-import uicCodes from 'uic-codes'
-import { fileURLToPath, URL } from 'url'
-import stationsMap from './_stations_json.js'
+import cleanStationName from "db-clean-station-name";
+import isUicLocationCode from "is-uic-location-code";
+import countries from "i18n-iso-countries";
+import fetch from "node-fetch";
+import uicCodes from "uic-codes";
+import { fileURLToPath, URL } from "url";
+import stationsMap from "./_stations_json.js";
 
-const fetchStations = async query => {
-	const urlA = new URL('https://v5.db.transport.rest/locations?poi=false&addresses=false')
-	const urlB = new URL('https://v5.db.api.bahn.guru/locations?poi=false&addresses=false')
-	urlA.searchParams.append('query', query)
-	urlB.searchParams.append('query', query)
+const fetchStations = async (query) => {
+  const urlA = new URL(
+    "https://v5.db.transport.rest/locations?poi=false&addresses=false"
+  );
+  const urlB = new URL(
+    "https://v5.db.api.bahn.guru/locations?poi=false&addresses=false"
+  );
+  urlA.searchParams.append("query", query);
+  urlB.searchParams.append("query", query);
 
-	return Promise.race([
-		fetch(urlA),
-		fetch(urlB),
-	]).then(res => res.json())
-}
+  return Promise.race([fetch(urlA), fetch(urlB)]).then((res) => res.json());
+};
 
-export const formatHafasStationId = i => (i.length === 9 && i.slice(0, 2)) ? i.slice(2) : i
+export const formatHafasStationId = (i) =>
+  i.length === 9 && i.slice(0, 2) ? i.slice(2) : i;
 
-const hafasStationIsLongDistanceOrRegionalOrSuburban = s => {
-	return s.products && (s.products.nationalExp || s.products.nationalExpress || s.products.national || s.products.regionalExp || s.products.regionalExpress || s.products.regional || s.products.suburban) && isUicLocationCode(formatHafasStationId(s.id))
-}
+const hafasStationIsLongDistanceOrRegionalOrSuburban = (s) => {
+  return (
+    s.products &&
+    (s.products.nationalExp ||
+      s.products.nationalExpress ||
+      s.products.national ||
+      s.products.regionalExp ||
+      s.products.regionalExpress ||
+      s.products.regional ||
+      s.products.suburban) &&
+    isUicLocationCode(formatHafasStationId(s.id))
+  );
+};
 
-const hafasStationIsNotRegion = hafasStation => {
-	return hafasStation.name.toUpperCase() !== hafasStation.name
-}
+const hafasStationIsNotRegion = (hafasStation) => {
+  return hafasStation.name.toUpperCase() !== hafasStation.name;
+};
 
-const fixHafasStationId = hafasStation => ({
-	...hafasStation,
-	id: formatHafasStationId(hafasStation.id),
-})
+const fixHafasStationId = (hafasStation) => ({
+  ...hafasStation,
+  id: formatHafasStationId(hafasStation.id),
+});
 
-const countryForStationId = id => {
-	if (!isUicLocationCode(id)) return undefined
-	const countryPrefix = +id.slice(0, 2)
-	const alpha3 = uicCodes.toISO[countryPrefix]
-	return alpha3 ? countries.alpha3ToAlpha2(alpha3) : undefined
-}
+const countryForStationId = (id) => {
+  if (!isUicLocationCode(id)) return undefined;
+  const countryPrefix = +id.slice(0, 2);
+  const alpha3 = uicCodes.toISO[countryPrefix];
+  return alpha3 ? countries.alpha3ToAlpha2(alpha3) : undefined;
+};
 
-const stationHasLocation = station => {
-	return !!station.location
-}
+const stationHasLocation = (station) => {
+  return !!station.location;
+};
 
-const fixStationName = station => ({
-	...station,
-	name: cleanStationName(station.name),
-})
+const fixStationName = (station) => ({
+  ...station,
+  name: cleanStationName(station.name),
+});
 
-const createStation = hafasStation => {
-	const matchingEntry = stationsMap.get(hafasStation.id)
-	return matchingEntry || {
-		id: hafasStation.id,
-		name: hafasStation.name, // todo
-		isMeta: false,
-		country: countryForStationId(hafasStation.id),
-		location: hafasStation.location
-			? {
-				longitude: hafasStation.location.longitude,
-				latitude: hafasStation.location.latitude,
-			}
-			: undefined,
-	}
-}
+const createStation = (hafasStation) => {
+  const matchingEntry = stationsMap.get(hafasStation.id);
+  return (
+    matchingEntry || {
+      id: hafasStation.id,
+      name: hafasStation.name, // todo
+      isMeta: false,
+      country: countryForStationId(hafasStation.id),
+      location: hafasStation.location
+        ? {
+            longitude: hafasStation.location.longitude,
+            latitude: hafasStation.location.latitude,
+          }
+        : undefined,
+    }
+  );
+};
 
 export default async function handler(req, res) {
-	const { query } = req.query
-	if (!query) return res.status(400).json({ error: true, message: 'missing `query` parameter' })
+  const { query } = req.query;
+  console.log(query);
+  if (!query)
+    return res
+      .status(400)
+      .json({ error: true, message: "missing `query` parameter" });
 
-	try {
-		const hafasStations = await fetchStations(query)
-		const stations = hafasStations
-			.filter(hafasStationIsLongDistanceOrRegionalOrSuburban)
-			.filter(hafasStationIsNotRegion)
-			.map(fixHafasStationId)
-			.map(createStation)
-			.filter(stationHasLocation)
-			.map(fixStationName)
-		return res.json(stations)
-	} catch (error) {
-		console.error(error)
-		return res.status(500).json({ error: true, message: 'internal error' })
-	}
+  try {
+    // const hafasStations = await fetchStations(query);
+    // console.log(hafasStations, hafasStations.filter);
+    // const stations = hafasStations
+    //   .filter(hafasStationIsLongDistanceOrRegionalOrSuburban)
+    //   .filter(hafasStationIsNotRegion)
+    //   .map(fixHafasStationId)
+    //   .map(createStation)
+    //   .filter(stationHasLocation)
+    //   .map(fixStationName);
+    const all_stations = [];
+    for (const [key, value] of stationsMap.entries()) {
+      // all_stations.push(value);
+      if (value.name.toLowerCase().includes(query.toLowerCase())) {
+        all_stations.push(value);
+      }
+    }
+    return res.json(all_stations);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: true, message: "internal error" });
+  }
 }
 
 // export const stationById = async (req, res) => {
--- is-eurail-cheaper/pages/api/trenitalia.js
+++ is-eurail-cheaper/pages/api/trenitalia.js
@@ -1,58 +1,66 @@
 // https://medium.com/@albigiu/trenitalia-shock-non-crederete-mai-a-queste-api-painful-14433096502c blogpost banger
 
-let station_search_url = (query) => `https://www.lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=${query}&limit=1`
+let station_search_url = (query) =>
+  `https://www.lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=${query}&limit=1`;
 
-let journey_search_url = "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions"
+let journey_search_url =
+  "https://www.lefrecce.it/Channels.Website.BFF.WEB/website/ticket/solutions";
 
 async function station(name) {
-    return await fetch(station_search_url(name))
-        .then(response => response.json())
-        .then(result => result[0]);
+  return await fetch(station_search_url(name))
+    .then((response) => response.json())
+    .then((result) => result[0]);
 }
 
 export default async function Trenitalia_price(legInfo) {
-    return Promise.all([await station(legInfo.fromCity), await station(legInfo.toCity)])
-        .then(cities => {
-            return fetch(journey_search_url, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "departureLocationId": parseInt(cities[0].id),
-                    "arrivalLocationId": parseInt(cities[1].id),
-                    "departureTime": "2023-11-28T18:00:00.000", // todo date
-                    "adults": 1,
-                    "children": 0,
-                    "criteria": {
-                        "frecceOnly": false,
-                        "regionalOnly": false,
-                        "intercityOnly": false,
-                        "noChanges": false,
-                        "order": "DEPARTURE_DATE",
-                        "offset": 0,
-                        "limit": 10
-                    },
-                    "advancedSearchRequest": {
-                        "bestFare": false,
-                        "bikeFilter": false
-                    }
-                })
-            })
-                .then(response => response.json())
-                .then(result => {
-                    let price = null;
-                    result.solutions.forEach(solution => {
-                        let s = solution.solution;
-                        // console.log("TI SOL", s.price, s.trains.map(i => [i.description, i.acronym]));
-                        // console.log("FOR", legInfo.line.fahrtNr);
-                        if (s.price !== null && s.trains.length === 1 && s.trains[0].description === legInfo.line.fahrtNr) {
-                            // console.log("taking price");
-                            price = { 'price': s.price.amount };
-                        }
-                    });
-                    return price;
-                });
-        })
+  return Promise.all([
+    await station(legInfo.fromCity),
+    await station(legInfo.toCity),
+  ]).then((cities) => {
+    return fetch(journey_search_url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        departureLocationId: parseInt(cities[0].id),
+        arrivalLocationId: parseInt(cities[1].id),
+        departureTime: "2023-11-28T18:00:00.000", // todo date
+        adults: 1,
+        children: 0,
+        criteria: {
+          frecceOnly: false,
+          regionalOnly: false,
+          intercityOnly: false,
+          noChanges: false,
+          order: "DEPARTURE_DATE",
+          offset: 0,
+          limit: 10,
+        },
+        advancedSearchRequest: {
+          bestFare: false,
+          bikeFilter: false,
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        let price = null;
+        result.solutions.forEach((solution) => {
+          let s = solution.solution;
+          // console.log("TI SOL", s.price, s.trains.map(i => [i.description, i.acronym]));
+          // console.log("FOR", legInfo.line.fahrtNr);
+          if (
+            s.price !== null &&
+            s.trains.length === 1 &&
+            s.trains[0].description === legInfo.line.fahrtNr
+          ) {
+            // console.log("taking price");
+            price = { price: s.price.amount };
+          }
+        });
+        return price;
+      });
+  });
 }
